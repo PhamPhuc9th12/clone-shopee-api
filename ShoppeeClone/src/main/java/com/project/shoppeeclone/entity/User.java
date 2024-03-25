5f3d52b939160fdff52a20851e9799d5c12c3978
PhucPT09 <pphuc9122002@gmail.com>
2024-03-25 21:04:09 +0700

    first commit

---
+++ ShoppeeClone/src/main/java/com/project/shoppeeclone/entity/User.java
@@ -0,0 +1,14 @@
+package com.project.shoppeeclone.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+    private Long id;
+}
