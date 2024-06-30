a514a42f398b24ea56f9cce5ad68ec893967ff41
PhamPhuc09 <pphuc9122002@gmail.com>
2024-06-30 16:22:59 +0700

    add user response

---
+++ ShoppeeClone/src/main/java/com/project/shoppeeclone/dto/response/UserResponse.java
@@ -0,0 +1,17 @@
+package com.project.shoppeeclone.dto.response;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class UserResponse {
+    private Long id;
+    private String fullName;
+    private String birthdayString;
+    private String gender;
+    private String email;
+    private String phone;
+}
